d3b281e2452aca69e645038ae79e9229f84888d4
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 14:22:39 +0000

    moved getauthtoken function into its own javascript file

---
+++ snoot-city/src/utilities/GetAuthToken.js
@@ -0,0 +1,19 @@
+
+async function GetAuthToken() {
+    const response = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+            client_id: import.meta.env.VITE_PETFINDER_CLIENT_ID,
+            client_secret: import.meta.env.VITE_PETFINDER_CLIENT_SECRET,
+        }),
+    });
+
+    const data = await response.json();
+    return data.access_token;
+};
+
+export default GetAuthToken;
