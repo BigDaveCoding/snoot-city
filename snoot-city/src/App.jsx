8d74bef0742fb28ce033429906c37418f253327a
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 14:24:24 +0000

    app.jsx handleNextPage function created which gets the next page of dogs. setting pagination inside handle search and handlenextpage

--- snoot-city/src/App.jsx
+++ snoot-city/src/App.jsx
@@ -5,23 +5,33 @@ import './App.css'
 import PetList from './components/PetList'
 import SearchPets from './components/SearchPets'
 import GetSighthounds from './utilities/petfinder-Api'
-
+import NextButton from './components/NextPrevButtons'
 function App() {
 
   const [searchData, setSearchData] = useState(null)
+  const [pagination, setPagination] = useState(null)
 
   const handleSearch = async () => {
     console.log("handleSearch")
     const data = await GetSighthounds()
-    setSearchData(data)
+    console.log(data.animals)
+    setSearchData(data.animals)
+    setPagination(data)
+    
+  }
+
+  const handleNextPage = (data) => {
     console.log(data)
+    setSearchData(data.animals)
+    setPagination(data)
   }
 
   return (
     <>
-      <p className="text-5xl text-red-500 underline underline-offset-10">testing</p>
+      {/* <p className="text-5xl text-red-500 underline underline-offset-10">testing</p> */}
       <SearchPets onSearch={handleSearch} />
       {searchData && <PetList data={searchData} />}
+      {pagination && <NextButton data={pagination} onNext={handleNextPage} /> }
       
     </>
   )
