eaf4e55b388942357629fb43745cc445b73f6dfa
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 14:46:35 +0000

    setting value of loading within handle next.placed logic inside a try catch

--- snoot-city/src/components/NextPrevButtons.jsx
+++ snoot-city/src/components/NextPrevButtons.jsx
@@ -1,16 +1,32 @@
 import GetAuthToken from "../utilities/GetAuthToken"
 
 
-export default function NextButton({data, onNext}) {
+export default function NextButton({data, onNext, loading}) {
 
     console.log(data)
 
     const handleNext = async (e) => {
         console.log(data.pagination._links.next.href)
+
+        loading(true)
+        
         e.preventDefault()
-        const nextPageData = await GetNextPageData(`https://api.petfinder.com${data.pagination._links.next.href}`)
-        console.log(nextPageData)
-        onNext(nextPageData)
+
+        if(data.pagination._links.next.href){
+            try{
+                const nextPageData = await GetNextPageData(`https://api.petfinder.com${data.pagination._links.next.href}`)
+                console.log(nextPageData)
+                onNext(nextPageData)
+    
+            } catch(err) {
+                console.error("next page link doesnt exist")
+            }
+        } else {
+            console.error("no next page")
+        }
+        
+        loading(false)
+        
     }
 
     return (
