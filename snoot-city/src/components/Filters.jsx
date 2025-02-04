270022abb26913ca3b052e4c65ab6b3f78e813e2
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 23:27:29 +0000

    had to change the way filters were handled in the Filters component. new handleFilterChange function and state

--- snoot-city/src/components/Filters.jsx
+++ snoot-city/src/components/Filters.jsx
@@ -3,12 +3,13 @@ import { useState } from "react"
 
 export default function Filters({onFilterChange}) {
 
-    const [gender, setGender] = useState("any")
+    const [filters, setFilters] = useState({ gender: "any", age: "any" });
 
-    const handleGenderChange = (e) => {
-        const selectedGender = e.target.value;
-        setGender(selectedGender);
-        onFilterChange({ gender: selectedGender });
+    const handleFilterChange = (e) => {
+        const { name, value } = e.target; // Get name and value from event
+        const newFilters = { ...filters, [name]: value }; // Merge filters
+        setFilters(newFilters);
+        onFilterChange(newFilters); // Send the updated filters
     };
 
     
@@ -17,10 +18,18 @@ export default function Filters({onFilterChange}) {
             <button className="border-2 p-2">Filters</button>
             <div className="p-2">
                 <label> Gender:
-                    <select className="border-1 p-2 rounded" name="pet_gender" id="pet_gender" onChange={handleGenderChange} >
+                    <select className="border-1 p-2 rounded" name="gender" value={filters.gender} onChange={handleFilterChange} >
                         <option value="any" defaultValue>Any</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
+                    </select>
+                </label>
+                <label> Age:
+                    <select className="border-1 p-2 rounded" name="age" value={filters.age} onChange={handleFilterChange} >
+                        <option value="any" defaultValue>Any</option>
+                        <option value="baby">Baby</option>
+                        <option value="young">Young</option>
+                        <option value="adult">Adult</option>
                     </select>
                 </label>
             </div>
