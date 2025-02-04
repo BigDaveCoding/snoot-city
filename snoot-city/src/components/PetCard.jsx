cae1bb1f9e2f10cbc991caae25748c9858089801
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 16:02:14 +0000

    added logic in pet card to handle next and previous photos whilst scrolling through them

--- snoot-city/src/components/PetCard.jsx
+++ snoot-city/src/components/PetCard.jsx
@@ -1,18 +1,57 @@
-
+import { useState } from "react"
 
 export default function PetCard({data}) {
 
     // console.log(data)
 
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0)
+    // console.log(currentPhoto)
+    const photo_array = data.photos || []
+    // console.log(photo_array)
+
+    const handleNextPhoto = (e) => {
+        e.preventDefault()
+        if(currentPhotoIndex + 1 === photo_array.length) {
+            setCurrentPhotoIndex(0)
+        } else {
+            setCurrentPhotoIndex(currentPhotoIndex + 1)
+        }
+    }
+
+    const handlePrevPhoto = (e) => {
+        e.preventDefault()
+        if(currentPhotoIndex - 1 < 0) {
+            setCurrentPhotoIndex(photo_array.length - 1)
+        } else {
+            setCurrentPhotoIndex(currentPhotoIndex - 1)
+        }
+
+    }
+
+
+
     return (
         <>
-            {data.photos[0] && <img src={data.photos[0].full} alt='' />}
-            <p>{data.name}</p>
-            <p>{data.age}</p>
-            <p>{data.description}</p>
-            <p>{data.status}</p>
-            <p>{data.coat}</p>
-            <a href={data.url} target="_blank">Link</a>
+            
+            <div className="border-1 rounded-lg p-2">
+                <div className="flex flex-col items-center justify-center">
+                    <div className="flex justify-center items-center max-w-[400px] max-h-[400px] overflow-hidden">
+                        {data.photos[0] ? <img className="object-contain w-full h-full min-h-[400px]" src={photo_array[currentPhotoIndex].full} alt='' /> : <p className="text-red-600 text-2xl">No Image</p>}
+                    </div>
+                    {photo_array.length > 1 && <div>
+                        <button onClick={handleNextPhoto} className="border-1 p-2 mx-2 my-2">next</button>
+                        <button onClick={handlePrevPhoto} className="border-1 p-2 mx-2 my-2">prev</button>
+                    </div>}
+                </div>
+                <p className = "text-3xl">{data.name}</p>
+                <p>My Age: {data.age}</p>
+                {data.breeds.mixed ? <p>I am a {data.breeds.primary} / {data.breeds.secondary}</p> : <p>I am a {data.breeds.primary}</p>}
+                {data.photos.length === 0 && <p>{data.description}</p>}
+                <p>{data.status}</p>
+                <p>{data.coat}</p>
+                <a href={data.url} target="_blank">Link</a>
+            </div>
+            
 
         </>
     )
