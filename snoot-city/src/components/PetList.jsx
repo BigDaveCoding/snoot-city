81ee5eb22d2f43ec0f00d42f0d593f9988333dc9
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 13:25:34 +0000

    petlist component runs through each pet in data passed into function and creates a PetCrad component

---
+++ snoot-city/src/components/PetList.jsx
@@ -0,0 +1,17 @@
+
+import { useEffect, useState } from "react";
+import GetSighthounds from "../utilities/petfinder-Api";
+import PetCard from "./PetCard";
+
+export default function PetList({data}) {
+
+    return (
+        <div className="grid grid-cols-1 gap-4">
+            {data.length > 0 ? (
+                data.map((pet) => <PetCard key={pet.id} data={pet} />)
+            ) : (
+                <p>Loading Snoots...</p>
+            )}
+        </div>
+    )
+}
