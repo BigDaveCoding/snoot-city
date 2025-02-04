0ed74336540e9912976613c086d35ecf5d4ccef8
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 14:57:59 +0000

    added logic to handle previous button

--- snoot-city/src/components/NextPrevButtons.jsx
+++ snoot-city/src/components/NextPrevButtons.jsx
@@ -8,36 +8,59 @@ export default function NextButton({data, onNext, loading}) {
     const handleNext = async (e) => {
         console.log(data.pagination._links.next.href)
 
-        loading(true)
-        
         e.preventDefault()
 
         if(data.pagination._links.next.href){
             try{
-                const nextPageData = await GetNextPageData(`https://api.petfinder.com${data.pagination._links.next.href}`)
+                loading(true)
+                const nextPageData = await GetNextPrevPageData(`https://api.petfinder.com${data.pagination._links.next.href}`)
                 console.log(nextPageData)
                 onNext(nextPageData)
+                loading(false)
+
     
             } catch(err) {
-                console.error("next page link doesnt exist")
+                console.error(err)
             }
         } else {
             console.error("no next page")
         }
-        
-        loading(false)
-        
+    }
+
+    const handlePrev = async (e) => {
+
+        console.log(data.pagination._links.previous)
+        e.preventDefault()
+
+        if (data.pagination._links.previous) {
+            try{
+                loading(true)
+                const prevPageData = await GetNextPrevPageData(`https://api.petfinder.com${data.pagination._links.previous.href}`)
+                console.log(prevPageData)
+                onNext(prevPageData)
+                loading(false)
+
+
+            } catch(err) {
+                console.log(err)
+
+            }
+        } else {
+            console.error("no prevous page")
+        }
+
     }
 
     return (
         <>
             <button onClick={handleNext} className="border rounded bg-green-800">Next</button>
+            <button onClick={handlePrev} className="border rounded bg-red-950">previous page</button>
         </>
     ) 
 
 }
 
-async function GetNextPageData(url){
+async function GetNextPrevPageData(url){
     const token = await GetAuthToken()
 
     const response = await fetch(url,
