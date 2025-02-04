d501b4603ffef4831bfe29fcfd61a041cfebd82f
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 22:35:22 +0000

    creating a header component

---
+++ snoot-city/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <>
+            <header className="grid grid-cols-[5fr_1fr] h-10 justify-center items-center p-2">
+                <h2 className="text-xl">Welcome!</h2>
+            </header>
+        </>
+    )
+}
