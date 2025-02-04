fe33994ed94f732e82c8006b5a4a44ff43dd4e8a
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-04 13:28:04 +0000

    importing components into app.jsx. giving SearchPets a property of onSearch which runs handleSearch. if searchData exists then petList component runs with data as searchData

--- snoot-city/src/App.jsx
+++ snoot-city/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PetList from './components/PetList'
+import SearchPets from './components/SearchPets'
+import GetSighthounds from './utilities/petfinder-Api'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [searchData, setSearchData] = useState(null)
+
+  const handleSearch = async () => {
+    console.log("handleSearch")
+    const data = await GetSighthounds()
+    setSearchData(data)
+    console.log(data)
+  }
 
   return (
     <>
       <p className="text-5xl text-red-500 underline underline-offset-10">testing</p>
+      <SearchPets onSearch={handleSearch} />
+      {searchData && <PetList data={searchData} />}
       
     </>
   )
