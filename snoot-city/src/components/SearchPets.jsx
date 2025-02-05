cc6434455b400fa4e038d22b42177a1f60a2fa10
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-05 20:02:09 +0000

    changed to a button rather than a submit input

--- snoot-city/src/components/SearchPets.jsx
+++ snoot-city/src/components/SearchPets.jsx
@@ -9,7 +9,8 @@ export default function SearchPets({onSearch}) {
 
     return (
         <>
-            <input onClick={handleSearch} type="submit" value="search" className="border-2 p-2" />
+            {/* <input onClick={handleSearch} type="submit" value="search" className="border-2 p-2" /> */}
+            <button onClick={handleSearch} className="border-1 p-2 ml-2 rounded">Search Snoots!</button>
         </>
     ) 
 
