a7c9f70ff689b90d47252d8ce52eff8d3a7dc240
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-05 22:43:24 +0000

    edited styling of search snoots button

--- snoot-city/src/components/SearchPets.jsx
+++ snoot-city/src/components/SearchPets.jsx
@@ -11,7 +11,7 @@ export default function SearchPets({onSearch}) {
         <>
             {/* <input onClick={handleSearch} type="submit" value="search" className="border-2 p-2" /> */}
             <div className="flex justify-center items-center">
-                <button onClick={handleSearch} className="border-2 bg-logo-font-color font-medium text-xl p-2 ml-2 rounded w-8/12 ">Search Snoots!</button>
+                <button onClick={handleSearch} className=" text-background-primary uppercase bg-logo-font-color font-medium text-xl p-2 ml-2 rounded-3xl w-8/12">Search Snoots!</button>
             </div>
         </>
     ) 
