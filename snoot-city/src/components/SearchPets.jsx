3ed20d12bdbbad46e66115045f1e6b8b7150e02d
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-06 11:16:59 +0000

    added a variable className so I can set the style of the button wherever I want to place it

--- snoot-city/src/components/SearchPets.jsx
+++ snoot-city/src/components/SearchPets.jsx
@@ -1,6 +1,6 @@
 
 
-export default function SearchPets({onSearch}) {
+export default function SearchPets({onSearch, className}) {
 
     const handleSearch = (e) => {
         e.preventDefault()
@@ -11,7 +11,7 @@ export default function SearchPets({onSearch}) {
         <>
             {/* <input onClick={handleSearch} type="submit" value="search" className="border-2 p-2" /> */}
             <div className="flex justify-center items-center">
-                <button onClick={handleSearch} className=" text-background-primary uppercase bg-logo-font-color font-medium text-xl p-2 ml-2 rounded-3xl w-8/12">Search Snoots!</button>
+                <button onClick={handleSearch} className={className}>Search Snoots!</button>
             </div>
         </>
     ) 
