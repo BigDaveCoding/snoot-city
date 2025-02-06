d0930b932f9db42ed2e3cd6a86a5b525e203b11d
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-06 13:48:39 +0000

    working on navbar component.

--- snoot-city/src/components/NavBar.jsx
+++ snoot-city/src/components/NavBar.jsx
@@ -1,13 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 
 export default function NavBar() {
     return (
         <>
-            <nav className="font-baloo">
+            <div className="grid grid-cols-2 bg-background-primary border-b-1">
+                <img className="px-4 " src="../../images/small_snoot_logo.png" alt='' />
+                <FontAwesomeIcon icon={faBars} className="justify-self-end self-center px-4 text-2xl" />
+            </div>
+            {/* <nav className="font-baloo text-2xl">
                 <Link to="/" >Home</Link>
                 <Link to="/about">About</Link>
-            </nav>
+            </nav> */}
         </>
     )
 }
