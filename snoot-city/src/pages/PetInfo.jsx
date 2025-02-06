05fe6eb456e373e0bee4f733bd082f54ac196794
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-06 21:41:22 +0000

    added an attriubutes object.entries.map to go through the attributes, display the key and use the boolean to display a green tick or red cross. Also added the PetInfoAge component

--- snoot-city/src/pages/PetInfo.jsx
+++ snoot-city/src/pages/PetInfo.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe, faPaw, faArrowRight, faArrowLeft, faBone } from "@fortawesome/free-solid-svg-icons"
+import { faGlobe, faPaw, faArrowRight, faArrowLeft, faBone, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons"
 import GetAuthToken from "../utilities/GetAuthToken"
+import PetInfoAge from "../components/PetInfoAge";
 
 
 
@@ -15,7 +16,10 @@ export default function PetInfo() {
     const [name, setName] = useState("")
     const [photos, setPhotos] = useState([])
     const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0)
+    const [age, setAge] = useState("")
     const [attributes, setAttributes] = useState([])
+    const [environment, setEnvironment] = useState([])
+    const [tags, setTags] = useState([])
 
     useEffect(() => {
         async function fetchData(){
@@ -30,9 +34,14 @@ export default function PetInfo() {
             const data = await response.json()
             console.log(data)
 
+            // console.log(data.animal.attributes)
+
             setName(data.animal.name)
             setPhotos(data.animal.photos)
+            setAge(data.animal.age)
             setAttributes(data.animal.attributes)
+            setEnvironment(data.animal.environment)
+            setTags(data.animal.tags)
 
         }
         fetchData()
@@ -64,7 +73,6 @@ export default function PetInfo() {
         <>
             {/* <p>Pet info page</p> */}
             <div className="font-baloo bg-background-primary">
-
                 <h1 className="text-5xl text-center text-logo-font-color py-4">Hi! I'm {name}</h1>
 
                 <div className="flex flex-col items-center justify-center">
@@ -72,11 +80,37 @@ export default function PetInfo() {
                     <div className="flex justify-center items-center w-full h-[400px] overflow-hidden mb-auto">
                         {photos[0] ? <img className="object-cover w-full h-full" src={photos[currentPhotoIndex].full} alt='' /> : <p className="text-red-600 text-2xl">No Image</p>}
                     </div>
+
                     {photos.length > 1 && <div>
                         <button onClick={handlePrevPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowLeft} /></button>
                         <button onClick={handleNextPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowRight} /></button>
                     </div>}
                 </div>
+
+                <div>
+                    <PetInfoAge age={age} className="text-3xl text-center text-secondary-color" />
+                </div>
+
+                <div className="grid grid-cols-2">
+
+                    {Object.entries(attributes).map(([key, value]) => {
+                        return (
+
+                            <div key={key}>
+                                <p className="capitalize">{key.replace("_", " ")}:
+                                    {value === true ? 
+                                        <FontAwesomeIcon icon={faCheck} className="text-secondary-color text-xl pl-2" /> 
+                                        : 
+                                        <FontAwesomeIcon icon={faXmark} className="text-logo-font-color text-xl pl-2" />
+                                    }
+                                </p>  
+                            </div>
+                        )
+                    })}
+                    
+                </div>
+
+
             </div>
 
         </>
