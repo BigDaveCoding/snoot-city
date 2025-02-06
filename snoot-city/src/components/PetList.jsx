a4ae9083a99cf7059906879bab3b124cf1abb830
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-06 17:36:50 +0000

    added some styling to the pet list componenet for larger screen sizes

--- snoot-city/src/components/PetList.jsx
+++ snoot-city/src/components/PetList.jsx
@@ -6,7 +6,7 @@ import PetCard from "./PetCard";
 export default function PetList({data}) {
 
     return (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-2 px-6">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-2 px-6 lg:grid-cols-3 lg:gap-2 lg:px-2">
             {data.length > 0 ? (
                 data.map((pet) => <PetCard key={pet.id} data={pet} />)
             ) : (
