ad83122f7135d313ea7090b7297bf9a1219a8922
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-06 18:39:26 +0000

    creating a new page component which will take id from params and use it to fetch information about a specific dog from fetch. Displaying this information on the page. copied over the photo functions to handle next and previous photos and also the div in return which contains the photos.

---
+++ snoot-city/src/pages/PetInfo.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faPaw, faArrowRight, faArrowLeft, faBone } from "@fortawesome/free-solid-svg-icons"
+import GetAuthToken from "../utilities/GetAuthToken"
+
+
+
+export default function PetInfo() {
+
+    const {id} = useParams()
+
+    console.log(id)
+
+    const [name, setName] = useState("")
+    const [photos, setPhotos] = useState([])
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0)
+    const [attributes, setAttributes] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const token = await GetAuthToken()
+            const response = await fetch(`https://api.petfinder.com/v2/animals/${id}`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                }
+            })
+            const data = await response.json()
+            console.log(data)
+
+            setName(data.animal.name)
+            setPhotos(data.animal.photos)
+            setAttributes(data.animal.attributes)
+
+        }
+        fetchData()
+        
+    }, [id])
+
+    const handleNextPhoto = (e) => {
+        e.preventDefault()
+        if(currentPhotoIndex + 1 === photos.length) {
+            setCurrentPhotoIndex(0)
+        } else {
+            setCurrentPhotoIndex(currentPhotoIndex + 1)
+        }
+    }
+
+    const handlePrevPhoto = (e) => {
+        e.preventDefault()
+        if(currentPhotoIndex - 1 < 0) {
+            setCurrentPhotoIndex(photos.length - 1)
+        } else {
+            setCurrentPhotoIndex(currentPhotoIndex - 1)
+        }
+
+    }
+
+    
+
+    return (
+        <>
+            {/* <p>Pet info page</p> */}
+            <div className="font-baloo bg-background-primary">
+
+                <h1 className="text-5xl text-center text-logo-font-color py-4">Hi! I'm {name}</h1>
+
+                <div className="flex flex-col items-center justify-center">
+                
+                    <div className="flex justify-center items-center w-full h-[400px] overflow-hidden mb-auto">
+                        {photos[0] ? <img className="object-cover w-full h-full" src={photos[currentPhotoIndex].full} alt='' /> : <p className="text-red-600 text-2xl">No Image</p>}
+                    </div>
+                    {photos.length > 1 && <div>
+                        <button onClick={handlePrevPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowLeft} /></button>
+                        <button onClick={handleNextPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowRight} /></button>
+                    </div>}
+                </div>
+            </div>
+
+        </>
+        
+    )
+}
