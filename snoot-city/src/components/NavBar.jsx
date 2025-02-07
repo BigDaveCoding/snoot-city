c001b85749aa17264c07458b171678f462ecc6bd
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-07 13:37:28 +0000

    styling nav bar

--- snoot-city/src/components/NavBar.jsx
+++ snoot-city/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     return (
         <>
             <div className="grid grid-cols-2 bg-background-primary border-b-1">
-                <img className="px-4 " src="../../images/small_snoot_logo.png" alt='' />
+                <img className="px-4" src="../../images/small_snoot_logo.png" alt='' />
                 <FontAwesomeIcon icon={faBars} className="justify-self-end self-center px-4 text-2xl" />
             </div>
             {/* <nav className="font-baloo text-2xl">
