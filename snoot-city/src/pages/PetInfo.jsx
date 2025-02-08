cc84356e799a65fddbb4f28735cbcb49384372d5
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-08 14:08:28 +0000

    adding indentations and spacing to make the code more readable

--- snoot-city/src/pages/PetInfo.jsx
+++ snoot-city/src/pages/PetInfo.jsx
@@ -82,30 +82,40 @@ export default function PetInfo() {
 
     return (
         <>
-            <div className="bg-background-primary min-h-[90dvh] px-2">
+            <div className="bg-background-primary min-h-[90dvh] px-2 font-baloo">
+
                 {loading && <p className="text-xl py-10">Loading Snoot Info! Might take a second...</p>}
+
                 {!loading &&
                     <div className="font-baloo bg-background-primary">
+
                         <h1 className="text-5xl text-center text-logo-font-color py-4">Hi! I'm {name}</h1>
+
                         <div className="flex flex-col items-center justify-center">
                 
                             <div className="flex justify-center items-center w-full h-[400px] overflow-hidden mb-auto">
                                 {photos[0] ? <img className="object-cover w-full h-full" src={photos[currentPhotoIndex].full} alt='' /> : <p className="text-red-600 text-2xl">No Image</p>}
                             </div>
+
                             {photos.length > 1 && <div>
                                 <button onClick={handlePrevPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowLeft} /></button>
                                 <button onClick={handleNextPhoto} className="bg-logo-font-color text-background-primary text-2xl rounded-[50%] w-14 h-14 mx-6 my-2"><FontAwesomeIcon icon={faArrowRight} /></button>
                             </div>}
+
                         </div>
+
                         <div>
                             <PetInfoAge age={age} className="text-3xl text-center text-secondary-color py-2" />
                         </div>
+
                         <div>
                             <PetInfoBreed breed={breed} className="text-xl py-2" />
                         </div>
+
                         <div>
                             {gender && <p className="text-xl py-2">Gender: {gender}</p>}
                         </div>
+
                         <div className="grid grid-cols-2 py-2">
                             {Object.entries(attributes).map(([key, value]) => {
                                 return (
@@ -122,6 +132,7 @@ export default function PetInfo() {
                             })}
                 
                         </div>
+
                         <div className="py-2 ">
                             <p className="text-xl text-logo-font-color">I Love to Snoot it up with...</p>
                             <div className="grid grid-cols-3">
@@ -140,8 +151,10 @@ export default function PetInfo() {
                                 })}
                             </div>
                         </div>
+
                     </div>
                 }
+                
             </div>
 
         </>
