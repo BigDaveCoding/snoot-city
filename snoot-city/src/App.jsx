2323882372384a56f4c59ea3ae6897f93fb6f3ff
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-08 19:33:37 +0000

    setup a hook for manual navigation usig useState. Passing set function into navBar and homepage. also passing value into homepage of useState.

--- snoot-city/src/App.jsx
+++ snoot-city/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
 import PetInfo from "./pages/PetInfo"
@@ -8,14 +8,17 @@ import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
 
 function App() {
+
+  const [manualNavigation, setManualNavigation] = useState(false)
+
   return (
     <StrictMode>
       <BrowserRouter>
         <div className="bg-background-primary">
           <div className="flex flex-col mx-auto max-w-[1280px]">
-            <NavBar />
+            <NavBar setManualNavigation={setManualNavigation} />
               <Routes>
-                <Route path="/" element={<Homepage />} />
+                <Route path="/" element={<Homepage manualNavigation={manualNavigation} setManualNavigation={setManualNavigation} />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/petinfo/:id" element={<PetInfo />} />
                 <Route path="/contact" element={<Contact />} />
