08228108fcef59b0dd9f36cccd8d4294bfecdf14
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-08 19:35:01 +0000

    onClick event listsner on logo and home link. sets manual navigation state in app.jsx to true.

--- snoot-city/src/components/NavBar.jsx
+++ snoot-city/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 
 
-export default function NavBar() {
+export default function NavBar({setManualNavigation}) {
 
     const [navMenuOpen, setNavMenuOpen] = useState(false)
     const menuRef = useRef(null)
@@ -36,7 +36,7 @@ export default function NavBar() {
         <>
             <div className="grid grid-cols-2 bg-background-primary border-b-1 max-h-[8dvh] min-h-[8dvh] items-center z-50">
                 <div>
-                    <Link to="/"><img className="px-4 w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-3/12" src="../../images/snoot_logo_small_two.png" alt='' /></Link>
+                    <Link onClick={() => {setManualNavigation(true)}} to="/"><img className="px-4 w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-3/12" src="../../images/snoot_logo_small_two.png" alt='' /></Link>
                 </div>
                 <div className="flex justify-end">
                     {!navMenuOpen && <FontAwesomeIcon onClick={handleNavMenuOpenClose} icon={faBars} className="justify-self-end self-center px-4 text-2xl cursor-pointer" />}
@@ -47,7 +47,7 @@ export default function NavBar() {
             
             <div ref={menuRef} className="absolute mt-[8dvh] max-w-[1280px] bg-background-primary w-full opacity-0">
                 <div className="flex flex-col">
-                    <Link onClick={handleNavMenuOpenClose} className="w-full border-b-2 border-logo-font-color text-right py-2 pr-3 text-2xl" to="/" >Home</Link>
+                    <Link onClick={() => {handleNavMenuOpenClose(), setManualNavigation(true)}} className="w-full border-b-2 border-logo-font-color text-right py-2 pr-3 text-2xl" to="/" >Home</Link>
                     <Link onClick={handleNavMenuOpenClose} className="w-full border-b-2 border-secondary-color text-right py-2 pr-3 text-2xl" to="/about" >About</Link>
                     <Link onClick={handleNavMenuOpenClose} className="w-full border-b-2 border-logo-font-color text-right py-2 pr-3 text-2xl" to="/contact" >Contact</Link>
 
