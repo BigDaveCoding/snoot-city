10fe10632cf1f43cdd7ac28e4052a14adbf5db98
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-08 13:06:16 +0000

    using useEffect to trigger on rerender controlled by state navMenuOpen. Depending on boolean, a gsap animation is applied to the nav menu to either open/close. Using navMenuOpen state to dictate whether the menu symbol is bars or a cross. placed these inside a div as the parent is a grid.

--- snoot-city/src/components/NavBar.jsx
+++ snoot-city/src/components/NavBar.jsx
@@ -1,32 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 
 
 export default function NavBar() {
 
     const [navMenuOpen, setNavMenuOpen] = useState(false)
+    const menuRef = useRef(null)
 
     function handleNavMenuOpenClose(){
-        setNavMenuOpen(!navMenuOpen)
+        setNavMenuOpen(!navMenuOpen);
     }
+
+    useEffect(() => {
+        if (navMenuOpen){
+            gsap.to(menuRef.current, {
+                y: 0,
+                opacity: 1,
+                duration: 0.5,
+                ease: "power3.out"
+            });
+        } else {
+            gsap.to(menuRef.current, {
+                y: "-200%",
+                opacity: 0,
+                duration: 0.5,
+                ease: "power3.in"
+            });
+        };
+    }, [navMenuOpen])
 
     return (
         <>
             <div className="grid grid-cols-2 bg-background-primary border-b-1">
                 <img className="px-4 w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12" src="../../images/snoot_logo_small_two.png" alt='' />
-                <FontAwesomeIcon onClick={handleNavMenuOpenClose} icon={faBars} className="justify-self-end self-center px-4 text-2xl" />
+                <div className="flex justify-end">
+                    {!navMenuOpen && <FontAwesomeIcon onClick={handleNavMenuOpenClose} icon={faBars} className="justify-self-end self-center px-4 text-2xl" />}
+                    {navMenuOpen && <FontAwesomeIcon onClick={handleNavMenuOpenClose} icon={faXmark} className="justify-self-end self-center px-4 text-2xl" />}
+                </div>
             </div>
             
-            {navMenuOpen && 
-            <div className="fixed bg-background-primary w-full ">
+            
+            <div ref={menuRef} className="absolute bg-background-primary w-full opacity-0">
                 <div className="flex flex-col">
                     <Link onClick={handleNavMenuOpenClose} className="w-full border-b-2 border-logo-font-color text-right py-2 pr-3 text-2xl" to="/" >Home</Link>
                     <Link onClick={handleNavMenuOpenClose} className="w-full border-b-2 border-secondary-color text-right py-2 pr-3 text-2xl" to="/about" >About</Link>
                 </div>
             </div>
-            }
+            
         </>
     )
 }
