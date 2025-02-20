1d6dce0f70d23d8c781d8fe691bdd247a3422a57
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-20 20:51:27 +0000

    created a card component that takes 4 properties. sets background image of the entire div, has a title and link overlay

---
+++ snoot-city/src/components/AboutCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function AboutCard({bgImageSrc = "", title = "", linkText = "", linkRoute = ""}) {
+
+    return (
+        <>
+            <div className="relative flex flex-col h-[300px] w-full border-6 border-logo-font-color/50">
+                <img className="absolute object cover h-full w-full " src={bgImageSrc} alt='' />
+                <div className="w-full h-full flex flex-col justify-between items-center py-4 z-10">
+                    <h4 className="text-2xl text-center mx-4 text-white bg-logo-font-color/30 rounded-2xl p-2 mb-4">{title}</h4>
+                    <Link className="cursor-pointer border-1 text-white bg-secondary-color p-4 rounded-2xl" to={linkRoute} >
+                        {linkText}
+                    </Link>
+                </div>
+
+            </div>
+        </>
+    )
+
+}
