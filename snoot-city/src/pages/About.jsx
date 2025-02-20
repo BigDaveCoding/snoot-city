b8e28b470a2ac1a1204fa64abc7c5c0ddcf26176
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-20 14:04:46 +0000

    added a video into the about page

--- snoot-city/src/pages/About.jsx
+++ snoot-city/src/pages/About.jsx
@@ -2,8 +2,19 @@
 
 export default function About() {
     return(
-        <div className="flex flex-col min-h-[88dvh]">
-            <p className="font-baloo">This is the about page</p>
+        <div className="flex flex-col min-h-[88dvh] font-baloo">
+            <div className="flex flex-col p-2 justify-center items-center">
+                <p className="text-secondary-color text-2xl">About</p>
+                <h1 className="text-5xl font-medium text-logo-font-color">Oh My Snoot!</h1>
+            </div>
+
+            <div className="flex justify-center items-center w-full p-4 bg-secondary-color/50">
+                <video className="rounded-3xl border-1 h-80 w-full object-cover" autoPlay muted loop >
+                    <source src="../../public/images/snoot-video-one.mp4" type="video/mp4" />
+                    Your browser does not support the video tag
+                </video>
+            </div>
+
         </div>
     )
 }
