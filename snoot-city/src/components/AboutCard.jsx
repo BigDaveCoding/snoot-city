d4a8c305600a44859a2d6d046371f6fd317d55f3
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-22 18:48:21 +0000

    added an absolute positioned div to asct as an overlay of image

--- snoot-city/src/components/AboutCard.jsx
+++ snoot-city/src/components/AboutCard.jsx
@@ -6,6 +6,9 @@ export default function AboutCard({bgImageSrc = "", title = "", linkText = "", l
         <>
             <div className="relative flex flex-col h-[300px] w-full border-6 border-logo-font-color/50">
                 <img className="absolute object cover h-full w-full " src={bgImageSrc} alt='' />
+                {/* overlay */}
+                <div className="absolute bg-black/20 w-full h-full"></div>
+                
                 <div className="w-full h-full flex flex-col justify-between items-center py-4 z-10">
                     <h4 className="text-2xl text-center mx-4 text-white bg-logo-font-color/30 rounded-2xl p-2 mb-4">{title}</h4>
                     <Link className="cursor-pointer border-1 text-white bg-secondary-color p-4 rounded-2xl" to={linkRoute} >
